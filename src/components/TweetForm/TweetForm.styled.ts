31b67152ebd1e80257d3037676be2ecf40c50b01
Roman <kseexy@mail.ru>
2021-12-12 12:03:52 +0300

    refactor tweet form

--- src/components/TweetForm/TweetForm.styled.ts
+++ src/components/TweetForm/TweetForm.styled.ts
@@ -1,63 +1,76 @@
-import styled from 'styled-components';
-import Grid from '@mui/material/Grid';
-import Avatar from '@mui/material/Avatar';
-import TextareaAutosize from '@mui/base/TextareaAutosize';
-import Divider from '@mui/material/Divider';
+import styled from "styled-components";
+import Grid from "@mui/material/Grid";
+import Avatar from "@mui/material/Avatar";
+import TextareaAutosize from "@mui/base/TextareaAutosize";
+import Divider from "@mui/material/Divider";
 
-export const SWrapper = styled(Grid).attrs(() => ({
-	container: true,
+const Container = styled(Grid).attrs(() => ({
+    container: true,
 }))`
-	padding: 10px 16px;
-	gap: 16px;
+    padding: 10px 16px;
+    gap: 16px;
 `;
 
-export const SAvatar = styled(Grid).attrs(() => ({
-	item: true,
-	component: Avatar,
+const SAvatar = styled(Grid).attrs(() => ({
+    item: true,
+    component: Avatar,
 }))`
-	width: 48px;
-	height: 48px;
+    width: 48px;
+    height: 48px;
 `;
 
-export const STweetFormWrapper = styled(Grid).attrs(() => ({
-	item: true,
+const FormWrapper = styled(Grid).attrs(() => ({
+    item: true,
 }))`
-	flex-grow: 1;
+    flex-grow: 1;
 `;
 
-export const STweetTextArea = styled(TextareaAutosize)`
-	width: 100%;
-	padding: 16px 0;
-	max-height: 300px;
+const TextArea = styled(TextareaAutosize)`
+    width: 100%;
+    padding: 16px 0;
+    max-height: 300px;
 
-	&,
-	&::placeholder {
-		font-size: 20px;
-	}
+    &,
+    &::placeholder {
+        font-size: 20px;
+    }
 
-	&::placeholder {
-		color: ${({ theme }) => theme.mode.palette.gray};
-	}
+    &::placeholder {
+        color: ${({ theme }) => theme.mode.palette.gray};
+    }
 `;
 
-export const SDivider = styled(Divider)`
-	margin-bottom: 10px;
+const SDivider = styled(Divider)`
+    margin-bottom: 10px;
 `;
 
-export const SActions = styled(Grid).attrs(() => ({
-	container: true,
-	justifyContent: 'space-between',
-	alignItems: 'center',
+const FormActions = styled(Grid).attrs(() => ({
+    container: true,
+    justifyContent: "space-between",
+    alignItems: "center",
 }))``;
 
-export const SButtonsWrapper = styled.div`
-	display: flex;
-	align-items: center;
+const ButtonsWrapper = styled.div`
+    display: flex;
+    align-items: center;
 `;
 
-export const SButtonsDivider = styled(Divider).attrs(() => ({
-	orientation: 'vertical',
-	flexItem: true,
+const ButtonsDivider = styled(Divider).attrs(() => ({
+    orientation: "vertical",
+    flexItem: true,
 }))`
-	margin: 5px 10px;
+    margin: 5px 10px;
 `;
+
+const styles = {
+    Container,
+    SAvatar,
+    FormWrapper,
+    TextArea,
+    SDivider,
+    FormActions,
+    ButtonsWrapper,
+    ButtonsDivider,
+};
+
+export default styles;
--- src/components/TweetForm/TweetForm.tsx
+++ src/components/TweetForm/TweetForm.tsx
@@ -1,75 +1,70 @@
-import React from 'react';
-import {
-	SWrapper,
-	SAvatar,
-	STweetFormWrapper,
-	STweetTextArea,
-	SDivider,
-	SActions,
-	SButtonsWrapper,
-	SButtonsDivider,
-} from './TweetForm.styled';
-import IconButton from '@mui/material/IconButton';
-import ImageIcon from '@mui/icons-material/ImageOutlined';
-import EmojiIcon from '@mui/icons-material/SentimentSatisfiedOutlined';
-import Button from '@mui/material/Button';
-import CircularProgress from '@mui/material/CircularProgress';
+import React from "react";
+import IconButton from "@mui/material/IconButton";
+import ImageIcon from "@mui/icons-material/ImageOutlined";
+import EmojiIcon from "@mui/icons-material/SentimentSatisfiedOutlined";
+import Button from "@mui/material/Button";
+import CircularProgress from "@mui/material/CircularProgress";
+import S from "./TweetForm.styled";
 
 const MAX_LENGTH = 300;
 
 const TweetForm: React.FC = (): React.ReactElement => {
-	const [value, setValue] = React.useState('');
+    const [value, setValue] = React.useState("");
 
-	const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
-		setValue(e.currentTarget.value);
-	};
+    const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+        setValue(e.currentTarget.value);
+    };
 
-	const progress = (value.length / 300) * 100;
+    const progress = (value.length / 300) * 100;
 
-	return (
-		<SWrapper>
-			<SAvatar />
-			<STweetFormWrapper>
-				<STweetTextArea
-					maxLength={MAX_LENGTH}
-					value={value}
-					onChange={handleChange}
-					placeholder="What's happening?"
-				/>
-				<SDivider />
+    return (
+        <S.Container>
+            <S.SAvatar />
+            <S.FormWrapper>
+                <S.TextArea
+                    maxLength={MAX_LENGTH}
+                    value={value}
+                    onChange={handleChange}
+                    placeholder="What's happening?"
+                />
+                <S.SDivider />
 
-				<SActions>
-					<div>
-						<IconButton>
-							<ImageIcon />
-						</IconButton>
+                <S.FormActions>
+                    <div>
+                        <IconButton>
+                            <ImageIcon />
+                        </IconButton>
 
-						<IconButton>
-							<EmojiIcon />
-						</IconButton>
-					</div>
+                        <IconButton>
+                            <EmojiIcon />
+                        </IconButton>
+                    </div>
 
-					<SButtonsWrapper>
-						{!!progress && (
-							<>
-								<CircularProgress
-									size={25}
-									variant='determinate'
-									value={progress}
-									color={progress === 100 ? 'warning' : 'secondary'}
-								/>
-								<SButtonsDivider />
-							</>
-						)}
+                    <S.ButtonsWrapper>
+                        {!!progress && (
+                            <>
+                                <CircularProgress
+                                    size={25}
+                                    variant="determinate"
+                                    value={progress}
+                                    color={
+                                        progress === 100
+                                            ? "warning"
+                                            : "secondary"
+                                    }
+                                />
+                                <S.ButtonsDivider />
+                            </>
+                        )}
 
-						<Button variant='contained' disabled={!value}>
-							Tweet
-						</Button>
-					</SButtonsWrapper>
-				</SActions>
-			</STweetFormWrapper>
-		</SWrapper>
-	);
+                        <Button variant="contained" disabled={!value}>
+                            Tweet
+                        </Button>
+                    </S.ButtonsWrapper>
+                </S.FormActions>
+            </S.FormWrapper>
+        </S.Container>
+    );
 };
 
 export default TweetForm;
