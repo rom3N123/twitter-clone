2903739fda7e9bb15a07345ecb5f54cd23cb21fc
Roman <kseexy@mail.ru>
2021-12-12 12:05:02 +0300

    refactor page header

--- src/components/PageComponents/PageHeader/PageHeader.styled.ts
+++ src/components/PageComponents/PageHeader/PageHeader.styled.ts
@@ -1,15 +1,19 @@
-import styled from 'styled-components';
-import Typography from '@mui/material/Typography';
+import styled from "styled-components";
+import Typography from "@mui/material/Typography";
 
-export const SHeader = styled.header`
-	height: 53px;
-	padding: 0 16px;
-	display: flex;
-	align-items: center;
-	border-bottom: 1px solid ${({ theme }) => theme.mode.divider};
+const Header = styled.header`
+    height: 53px;
+    padding: 0 16px;
+    display: flex;
+    align-items: center;
+    border-bottom: 1px solid ${({ theme }) => theme.mode.divider};
 `;
 
-export const SHeaderTitle = styled(Typography)`
-	font-weight: 700;
-	font-size: 20px;
+const Title = styled(Typography)`
+    font-weight: 700;
+    font-size: 20px;
 `;
+
+const styles = { Header, Title };
+
+export default styles;
--- src/components/PageComponents/PageHeader/PageHeader.tsx
+++ src/components/PageComponents/PageHeader/PageHeader.tsx
@@ -1,18 +1,21 @@
-import React from 'react';
-import { SHeader, SHeaderTitle } from './PageHeader.styled';
+import React from "react";
+import S from "./PageHeader.styled";
 
 interface IPageHeaderProps {
-	title?: string;
-	children?: React.ReactNode;
+    title?: string;
+    children?: React.ReactNode;
 }
 
-const PageHeader: React.FC<IPageHeaderProps> = ({ title, children }): React.ReactElement => {
-	return (
-		<SHeader>
-			{title && <SHeaderTitle>{title}</SHeaderTitle>}
-			{children}
-		</SHeader>
-	);
+const PageHeader: React.FC<IPageHeaderProps> = ({
+    title,
+    children,
+}): React.ReactElement => {
+    return (
+        <S.Header>
+            {title && <S.Title>{title}</S.Title>}
+            {children}
+        </S.Header>
+    );
 };
 
 export default PageHeader;
