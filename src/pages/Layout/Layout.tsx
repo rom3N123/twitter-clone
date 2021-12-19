f6658a0793428874c4ba189ea11e8ad2a39a80af
Roman <kseexy@mail.ru>
2021-12-19 17:43:00 +0300

    add lazy loading for components

--- src/pages/Layout/Layout.tsx
+++ src/pages/Layout/Layout.tsx
@@ -1,36 +1,40 @@
-import React from "react";
+import React, { Suspense } from "react";
 import Container from "@mui/material/Container";
 import Grid from "@mui/material/Grid";
 import Navigation from "@components/Navigation";
 import Searchbar from "@components/PageComponents/Searchbar";
 import S from "./Layout.styled";
 import { Routes, Route } from "react-router-dom";
-import Home from "../Home";
-import Profile from "../Profile";
+import { CircularProgress } from "@mui/material";
 
-const Layout = () => {
+const Home = React.lazy(() => import("../Home"));
+const Profile = React.lazy(() => import("../Profile"));
+
+const Layout: React.FC = (): React.ReactElement => {
     return (
         <section>
-            <Container maxWidth="lg">
-                <Grid container>
-                    <S.SideContent xs={3}>
-                        <Navigation />
-                    </S.SideContent>
+            <Suspense fallback={<CircularProgress />}>
+                <Container maxWidth="lg">
+                    <Grid container>
+                        <S.SideContent xs={3}>
+                            <Navigation />
+                        </S.SideContent>
 
-                    <S.MainContent xs={6}>
-                        <Routes>
-                            <Route path="/" element={<Home />} />
-                            <Route path="/profile" element={<Profile />} />
-                        </Routes>
-                    </S.MainContent>
+                        <S.MainContent xs={6}>
+                            <Routes>
+                                <Route path="/" element={<Home />} />
+                                <Route path="/profile" element={<Profile />} />
+                            </Routes>
+                        </S.MainContent>
 
-                    <S.SideContent sx={{ paddingLeft: "20px" }} xs={3}>
-                        <Routes>
-                            <Route path="/" element={<Searchbar />}></Route>
-                        </Routes>
-                    </S.SideContent>
-                </Grid>
-            </Container>
+                        <S.SideContent sx={{ paddingLeft: "20px" }} xs={3}>
+                            <Routes>
+                                <Route path="/" element={<Searchbar />}></Route>
+                            </Routes>
+                        </S.SideContent>
+                    </Grid>
+                </Container>
+            </Suspense>
         </section>
     );
 };
