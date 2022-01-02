6af4b5e8919af6fba6da23a8bcc9e22521e1734c
Martynov <r.martynov@gk-adept.ru>
2022-01-02 15:50:52 +0300

    feat: make selectors for user state

--- src/redux/ducks/user/index.ts
+++ src/redux/ducks/user/index.ts
@@ -1,6 +1,9 @@
 import userReducer from "./reducer";
 import { setUserAction } from "./actions";
+import { selectUserState } from "./selectors";
 
 export { setUserAction };
+
+export { selectUserState };
 
 export default userReducer;
---
+++ src/redux/ducks/user/selectors.ts
@@ -0,0 +1,3 @@
+import { RootState } from "@redux/store";
+
+export const selectUserState = ({ user }: RootState) => user;
